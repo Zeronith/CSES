67cb66be16295e97b1d4fb1b4dd8224e73d716e2
Zeronith <kaizen52448380730@gmail.com>
2025-04-19 13:03:22 +0800

    Adding new problem

--- CoinPiles.cpp
+++ CoinPiles.cpp
@@ -1,37 +1,19 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main ()
-{
-  long long int t;
-  cin >> t;
-  while(t--)
-  {
-    long long int a, b;
-    cin >> a >> b;
-    while(a>0 || b>0)
-    {
-      if(a>b)
-      {
-        int m = min(a/2, b);
-        a=a-m;
-        b=b-m;
-      }
-      else
-      {
-        int m = min(b/2, a);
-        b=b-m;
-        a=a-m;
-      }
-      // cout << a <<" " << b << endl;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        long long a, b;
+        cin >> a >> b;
+        long long sum = a + b;
+        long long mi = min(a, b);
+        long long ma = max(a, b);
+        
+        if(sum % 3 == 0 && mi * 2 >= ma)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
     }
-    if(a==b && a==0)
-    {
-      cout << "YES" <<endl;
-    }
-    else
-    {
-      cout << "NO" << endl;
-    }
-  }
- 
 }
